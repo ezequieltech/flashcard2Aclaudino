6eed4c327dce6daf42f3e19918bed82234cceed1
ezequieltech <ezequiel.tech@escola.pr.gov.br>
2025-07-07 11:38:31 -0300

    gh

---
+++ perguntas.js
@@ -0,0 +1,17 @@
+criaCartao(
+    geografia,
+    Qual a capital da França,
+    A capital da França é Paris
+)
+
+criaCartao(
+    categoria,
+    pergunta,
+    resposta
+)
+
+criaCartao(
+    categoria,
+    pergunta,
+    resposta
+)
