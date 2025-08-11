da8a203e74dbf929523b9050a3f0e0c84fa3d0ff
ezequieltech <ezequiel.tech@escola.pr.gov.br>
2025-08-11 09:12:20 -0300

    sss

--- perguntas.js
+++ perguntas.js
@@ -1,17 +1,12 @@
 criaCartao(
-    geografia,
-    Qual a capital da França,
-    A capital da França é Paris
+    'geografia',
+    'Qual a capital da França',
+        'A capital da França é Paris'
 )
 
 criaCartao(
-    categoria,
-    pergunta,
-    resposta
+    'categoria',
+    'pergunta',
+    'resposta'
 )
 
-criaCartao(
-    categoria,
-    pergunta,
-    resposta
-)
